e6b06c1bdbd5fe1c1c6bb9d1e405e5a24ce4a510
Donovan Daniels <dwddaniels262@gmail.com>
2021-02-09 14:10:08 -0600

    setup db script

---
+++ scripts/setup-db.ts
@@ -0,0 +1,191 @@
+import { mdb } from "../src/db";
+import { User } from "../src/db/models";
+import { USER_FLAGS } from "../src/util/Constants";
+import * as fs from "fs-extra";
+import config from "../src/config";
+
+const drop = true, removeFiles = true;
+
+async function setupAlbums() {
+	const col = mdb.collection("albums");
+	if (!drop) await col.dropIndexes().then(() => console.log(`Dropped all of the indexes on \`${mdb.databaseName}\`.\`albums\`.`));
+	await col.createIndexes([
+		{
+			name: "id",
+			key: {
+				id: 1
+			},
+			unique: true
+		},
+		{
+			name: "title",
+			key: {
+				title: 1
+			},
+			unique: false
+		},
+		{
+			name: "vanity",
+			key: {
+				vanity: 1
+			},
+			unique: true,
+			partialFilterExpression: {
+				vanity: {
+					$type: "string"
+				}
+			}
+		},
+		{
+			name: "tags",
+			key: {
+				tags: 1
+			},
+			unique: false
+		}
+	]);
+}
+
+async function setupImages() {
+	const col = mdb.collection("images");
+	if (!drop) await col.dropIndexes().then(() => console.log(`Dropped all of the indexes on \`${mdb.databaseName}\`.\`images\`.`));
+	await col.createIndexes([
+		{
+			name: "id",
+			key: {
+				id: 1
+			},
+			unique: true
+		},
+		{
+			name: "file.md5",
+			key: {
+				"file.md5": 1
+			},
+			unique: true
+		},
+		{
+			name: "tags",
+			key: {
+				tags: 1
+			},
+			unique: false
+		},
+		{
+			name: "uploader",
+			key: {
+				uploader: 1
+			},
+			unique: false
+		},
+		{
+			name: "album",
+			key: {
+				album: 1
+			},
+			unique: false
+		},
+		{
+			name: "rating",
+			key: {
+				rating: 1
+			},
+			unique: false
+		}
+	]);
+}
+
+async function setupUsers() {
+	const col = mdb.collection("users");
+	if (!drop) await col.dropIndexes().then(() => console.log(`Dropped all of the indexes on \`${mdb.databaseName}\`.\`users\`.`));
+	await col.createIndexes([
+		{
+			name: "id",
+			key: {
+				id: 1
+			},
+			unique: true
+		},
+		{
+			name: "handle",
+			key: {
+				handle: 1
+			},
+			unique: true
+		},
+		{
+			name: "email",
+			key: {
+				email: 1
+			},
+			unique: true,
+			partialFilterExpression: {
+				email: {
+					$type: "string"
+				}
+			}
+		},
+		{
+			name: "name",
+			key: {
+				name: 1
+			},
+			unique: false
+		}
+	]);
+}
+
+process.nextTick(async () => {
+	if (drop) {
+		console.log(`This WILL drop the \`${mdb.databaseName}\` database, exit within 5 seconds to cancel!`);
+		await new Promise((a, b) => setTimeout(a, 5e3));
+		console.log("Continuing..");
+		await mdb.dropDatabase();
+	}
+
+
+	await setupAlbums().then(() => console.log(`\`${mdb.databaseName}\`.\`albums\` setup successfully.`));
+	await setupImages().then(() => console.log(`\`${mdb.databaseName}\`.\`images\` setup successfully.`));
+	await setupUsers().then(() => console.log(`\`${mdb.databaseName}\`.\`users\` setup successfully.`));
+
+	await User.create({
+		flags: USER_FLAGS.STAFF + USER_FLAGS.ADMIN,
+		handle: "admin",
+		name: "Administrator",
+		email: "admin@yiff.guru",
+		emailVerified: true
+	}).then(async (u) => {
+		await u.setPassword("P@ssw0rd");
+		console.log(`Added admin user (id: ${u.id})`);
+	});
+
+	await User.create({
+		flags: USER_FLAGS.ARTIST,
+		handle: "anonymous",
+		name: "Anonymous",
+		email: "anonymous@yiff.guru",
+		emailVerified: true
+	}).then(u => console.log(`Added anonymous user (id: ${u.id})`));
+
+	await User.create({
+		flags: USER_FLAGS.ARTIST,
+		handle: "mixed-artists",
+		name: "Mixed Artists",
+		email: "mixed-artists@yiff.guru",
+		emailVerified: true
+	}).then(u => console.log(`Added mixed-artists user (id: ${u.id})`));
+
+	if (removeFiles) {
+		fs.readdirSync(config.dir.albums).map(v => {
+			if (v === ".gitkeep") return;
+			else {
+				fs.rmSync(`${config.dir.albums}/${v}`, {
+					recursive: true
+				});
+			}
+		});
+		console.log(`Removed old image files.`);
+	}
+
+	process.exit(0);
+});
